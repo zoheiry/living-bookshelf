ba273d474011c92b8ac1a126615ddc47c7517b93
Ali Elzoheiry <ali@Alis-MacBook-Pro.local>
2026-01-11 10:53:42 +0100

    Fix issue with VITE_API_URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
+const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:5001';
 
 // Helper to get headers
 const getHeaders = (token) => {
